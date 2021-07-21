e3232b65ac7f0d604ad7851663307d251eabce01
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-21 09:19:19 -0500

    finished school practice

---
+++ app.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const express = require('express')
+const db = require('./model/trips');
+const trips = require('./model/trips');
+const { title } = require('process');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let id = 3
+
+const app = express();
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const server = http.createServer(app)
+
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res) => {
+    res.render('home', {
+        title: "Trips",
+        trips: trips,
+    })
+})
+
+app.get('/new', (req,res) => {
+    res.render('new', {
+        title: "New Trip"
+    })
+})
+
+app.post('/new', (req,res) => {
+    const newTrip = {
+        id: id++,
+        title: req.body.city_location,
+        departure_date: req.body.departure,
+        return_date: req.body.return
+    }
+    trips.push(newTrip)
+    // console.log(newTrip)
+    res.redirect('/')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running: http://${hostname}:${port}`)
+})
---
+++ model/trips.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'Savannah',
+        departure_date: '2022-01-01',
+        return_date: '2022-01-03'
+    },
+    {
+        id: 2,
+        title: 'New York',
+        departure_date: '2022-02-23',
+        return_date: '2022-02-27'
+    },
+];
